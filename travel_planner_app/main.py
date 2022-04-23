f33675d1ecca5a78c146b0bce8c6a675bba51b6b
ikenney3 <ikenney3@huskers.unl.edu>
2022-04-23 15:59:00 -0500

    Added airport.csv for validations.

--- travel_planner_app/main.py
+++ travel_planner_app/main.py
@@ -1,27 +1,49 @@
 from kivy.app import App
 from kivy.modules import inspector
 from kivy.core.window import Window
+from travel_planner_app.database import Database
 from travel_planner_app.rest import RESTConnection
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 from api_key import API_KEY
-
-Persisted = declarative_base()
+from database import Airport, City, Venue
 
 
 class TravelPlannerApp(App):
-    def __init__(self, authority='localhost', port=33060, database='codeblue', username='root', password='cse1208',
+    def __init__(self, authority='localhost', port=33060, database='database', username='root', password='cse1208',
                  port_api=443, api_key=API_KEY, **kwargs):
         super(TravelPlannerApp, self).__init__(**kwargs)
         url = construct_mysql_url(authority, port, database, username, password)
-        self.database = TravelPlannerDatabase(url)
+        self.database = Database(url)
         self.session = self.database.create_session()
         self.connection = RESTConnection('api.openweathermap.org', port_api, '/data/2.5')
         self.api_key = api_key
 
     def build(self):
         inspector.create_inspector(Window, self)
+
+    def get_places_to_validate(self):
+        unvalidated_airports = self.session.query(Airport).all()
+        unvalidated_cities = self.session.query(City).all()
+        # for airport in range(len(unvalidated_airports)):
+        #     unvalidated_airports[airport] = unvalidated_airports[airport].name
+        # for city in range(len(unvalidated_cities)):
+        #     unvalidated_airports[city] = unvalidated_airports[city].name
+        return unvalidated_airports, unvalidated_cities
+
+    def get_venues_to_validate(self):
+        unvalidated_venues = self.session.query(Venue).all()
+        return unvalidated_venues
+    
+    def validate_place(self):
+        pass
+
+    def get_average_rating(self):
+        pass
+
+    def get_new_rating(self):
+        pass
+
+    def update_ratings(self):
+        pass
 
 
 def construct_mysql_url(authority, port, database, username, password):
@@ -30,21 +52,6 @@ def construct_mysql_url(authority, port, database, username, password):
 
 def construct_in_memory_url():
     return 'sqlite:///'
-
-
-# This is just a temporary class to help me test code, I'll use the remote database later.
-class TravelPlannerDatabase:
-    def __init__(self, url):
-        self.engine = create_engine(url)
-        self.Session = sessionmaker()
-        self.Session.configure(bind=self.engine)
-
-    def ensure_tables_exist(self):
-        Persisted.metadata.create_all(self.engine)
-
-    def create_session(self):
-        return self.Session()
-# End Temporary Code.
 
 
 def main():
