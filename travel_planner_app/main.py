f18e6e5a791a8861770bd77f21c3707c20a4b558
ikenney3 <ikenney3@huskers.unl.edu>
2022-04-24 15:08:19 -0500

    Started creating itinerary functions in main.

--- travel_planner_app/main.py
+++ travel_planner_app/main.py
@@ -1,3 +1,5 @@
+import math
+
 from kivy.app import App
 from kivy.modules import inspector
 from kivy.core.window import Window
@@ -20,6 +22,7 @@ class TravelPlannerApp(App):
         self.connection = RESTConnection('api.openweathermap.org', port_api, '/data/2.5')
         self.api_key = api_key
         self.validate_city_records = None
+        self.current_location = None
 
     def build(self):
         inspector.create_inspector(Window, self)
@@ -64,6 +67,7 @@ class TravelPlannerApp(App):
     def on_records_loaded(self, _, response):
         print(dumps(response, indent=4, sort_keys=True))
         self.validate_city_records = response
+        # call method that gets lat/long from GUI and validates location.
 
     def on_records_not_loaded(self, _, error):
         Logger.error(f'{self.__class__.__name__}: {error}')
@@ -75,6 +79,70 @@ class TravelPlannerApp(App):
         pass
 
     def update_ratings(self):
+        pass
+
+    def find_closest_airport_to_destination(self, in_range_airports, destination):
+        best_option = None
+        max_distance = 0
+        for airport in in_range_airports:
+            if self.find_distance(airport.latitude, airport.longitude, destination.latitude, destination.longitude) > max_distance:
+                max_distance = self.find_distance(airport.latitude, airport.longitude, destination.latitude, destination.longitude)
+                best_option = airport
+        return best_option
+
+    def get_airports_in_range(self, current_airport):
+        airports = self.session.query(Airport).all()
+        in_range_airports = []
+        for airport in airports:
+            if self.find_distance(current_airport.latitude, current_airport.longitude, airport.latitude, airport.longitude) <= 3500 and self.is_weather_ok_airport():
+                in_range_airports.append(airport)
+        return in_range_airports
+
+    def is_weather_ok_airport(self):
+        pass
+
+    def find_distance(self, current_latitude, current_longitude, next_latitude, next_longitude):
+        # need to figure out how to calculate whether you are still going East or West.
+        distance = math.acos(math.sin(current_latitude) * math.sin(next_latitude) +
+                             math.cos(current_latitude) * math.cos(next_latitude) *
+                             math.cos(next_longitude - current_longitude)) * 6371
+        return distance
+
+    def determine_best_city(self, airport):
+        best_city = None
+        city_score = 0
+        for city in airport.cities:
+            pass
+        return best_city
+
+    def get_city_score(self, city):
+        score = 0
+        venues_open = 0
+        if self.is_weather_good_city():
+            score += 3
+        for venue in city.venues:
+            if self.does_weather_meet_venues_conditions(venue, city.forecast):
+                venues_open += 1
+        score += venues_open
+        return score
+
+    def is_weather_good_city(self):
+        pass
+
+    def does_weather_meet_venues_conditions(self, venue, weather):
+        return True
+
+    def create_closest_itinerary_day(self, destination):
+        airport = self.find_closest_airport_to_destination(self.get_airports_in_range(self.current_location), destination)
+        pass
+
+    def create_entertainment_itinerary(self):
+        pass
+
+    def get_previous_itinerary(self):
+        pass
+
+    def get_current_location(self):
         pass
 
 
