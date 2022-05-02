31c834b2012ce34389dcbfa8ac2ef01bd04ab411
Kierin Andrews <kandrews4@huskers.unl.edu>
2022-05-01 22:07:20 -0500

    Fixed bug where app would crash after attempting an empty name then duplicate name for an edited venue

--- entertainment_tracking_app/main.py
+++ entertainment_tracking_app/main.py
@@ -25,7 +25,7 @@ def bad_lat_long(lat, long):
 class EntertainmentTrackerApp(App):
     def __init__(self, **kwargs):
         super(EntertainmentTrackerApp, self).__init__(**kwargs)
-        url = Database.construct_mysql_url('localhost', 3306, 'entertainment', 'root', 'cse1208')
+        url = Database.construct_mysql_url('localhost', 3306, 'airports', 'root', 'cse1208')
         self.entertainment_database = Database(url)
         self.session = self.entertainment_database.create_session()
         print('success')
@@ -72,7 +72,6 @@ class EntertainmentTrackerApp(App):
         return query.count() > 0
 
     def duplicate_name_venue(self, original_name, candidate_name, city_selection, create_or_edit):
-        message = f'A venue under the name {candidate_name} already exists in the chosen city.'
         duplicate_name = False
         city = self.session.query(City).filter(City.city_name == city_selection).one()
         # venues_to_check = self.session.query(Venue).filter(Venue.city_id == c_id)
@@ -80,10 +79,10 @@ class EntertainmentTrackerApp(App):
         for venue in venues_to_check:
             if create_or_edit == 'CREATE' and venue.venue_name == candidate_name:
                 duplicate_name = True
-                self.root.ids.venue_name_error.text = message
+                self.root.ids.venue_name_error.text = f'A venue under the name {candidate_name} already exists in the chosen city.'
             if create_or_edit == 'EDIT' and venue.venue_name == candidate_name and original_name != candidate_name:
                 duplicate_name = True
-                self.root.ids.venue_edit_message = message
+                self.root.ids.venue_edit_message.text = f'A venue under the name {candidate_name} already exists in the chosen city.'
         return duplicate_name
 
     def check_city_for_venues(self, city, edit_or_review):
